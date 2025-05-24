59bfb5d10068b447eccc9aca3c375248b0b74dc7
pear-pps <putsaya.slk@gmail.com>
2025-05-24 16:58:21 +0700

    Add files via upload

---
+++ Exercise4_Putsaya_S.py
@@ -0,0 +1,32 @@
+'''Foundation English
+General Business
+Introduction to Computer Systems
+Computer Programming'''
+'''-------------------------------------------------------------------------------------------------------------'''
+print()
+print("Tell me YOUR SCORE this semester")
+
+'''Foundation English'''
+FE = float(input("FE(25Point) : "))
+
+'''General Business'''
+GB = float(input("GB(25Point) : "))
+
+'''Introduction to Computer Systems'''
+ICS = float(input("ICS(25Point) : "))
+
+'''Computer Programming'''
+CP = float(input("CP(25Point) : "))
+'''-------------------------------------------------------------------------------------------------------------'''
+print()
+print("--- Your Score ---")
+
+print("Foundation English :",FE,"point")
+
+print("General Business :",GB,"point")
+
+print("Introduction to Computer Systems :",ICS,"point")
+
+print("Computer Programming :",CP,"point")
+print()
+print("--- Your TOTAL Score is ",FE+GB+ICS+CP,"/100.0 Point---")
