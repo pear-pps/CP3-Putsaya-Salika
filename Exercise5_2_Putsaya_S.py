ae8797c884d85cc21dcc77c767d2872e68a38bcc
pear-pps <putsaya.slk@gmail.com>
2025-05-25 15:27:05 +0700

    Create Exercise5_2_Putsaya_S.py

---
+++ Exercise5_2_Putsaya_S.py
@@ -0,0 +1,15 @@
+'''
+To calculate how fast can you go per time with physic formula
+From V = S / T
+V = Velocity(fastness)
+S = Displacement(move)
+T = Time
+'''
+'''------------------------------------------------------------------------------------------------------------'''
+print("--------------Speed Calculation---------------")
+s = int(input("Displacement(km): "))
+t = int(input("Time(hr): "))
+speed = int(s/t)
+print("-Your speed calculation is ",speed,"km/hr-")
+
+
