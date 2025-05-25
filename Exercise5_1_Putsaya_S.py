c8cbcdb771f80d3d9f096d5c47e5ca8d19da6258
pear-pps <putsaya.slk@gmail.com>
2025-05-25 14:43:58 +0700

    Create Exercise5_1_Putsaya_S.py

---
+++ Exercise5_1_Putsaya_S.py
@@ -0,0 +1,15 @@
+print("---------------Calculate your Number---------------")
+firstNum = int(input("Fist Number: "))
+secondNum = int(input("Second Number: "))
+plus = firstNum + secondNum
+minus = firstNum - secondNum
+multiply = firstNum * secondNum
+divide = int(firstNum / secondNum)
+print("--------------Calculated---------------")
+print(firstNum,"+",secondNum,"=",plus)
+print(firstNum,"-",secondNum,"=",minus)
+print(firstNum,"*",secondNum,"=",multiply)
+print(firstNum,"/",secondNum,"=",divide)
+
+
+
