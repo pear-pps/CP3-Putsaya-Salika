cf587282107dfa8408e560eacedb03bfb17d096f
pear-pps <putsaya.slk@gmail.com>
2025-05-28 14:37:01 +0700

    Create Lecture50_Putsaya_S.py

---
+++ Lecture50_Putsaya_S.py
@@ -0,0 +1,13 @@
+
+firstNumber = int(input("1st number: ")) # user enter first number
+secondNumber = int(input("2nd number: ")) # user enter second number
+
+def addNumber(x,y):
+    print(firstNumber+secondNumber)
+def calculateNumber(x,y):
+    print(firstNumber-secondNumber)
+    print(firstNumber*secondNumber)
+    print(firstNumber/secondNumber)
+
+addNumber(firstNumber,secondNumber)
+calculateNumber(firstNumber,secondNumber)
